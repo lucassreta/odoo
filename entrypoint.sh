0b610154fbcc5f75f8319f0757061803056f3f2f
lucassreta <consultas.lmcomputacion@gmail.com>
2025-09-02 13:06:04 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,19 @@ set -e
 
 : "${ODOO_ADMIN_PASSWORD:?Debes definir ODOO_ADMIN_PASSWORD en Railway}"
 
-# Reemplaza placeholder en odoo.conf con el valor real
+# El script ya tiene toda la información de la BD
+# No es necesario modificar odoo.conf para la conexión
+# `sed` para ODOO_ADMIN_PASSWORD está bien
 sed -i "s|\${ODOO_ADMIN_PASSWORD}|${ODOO_ADMIN_PASSWORD}|g" /etc/odoo/odoo.conf
 
-exec odoo -c /etc/odoo/odoo.conf \
-  --db_host="${PGHOST}" \
-  --db_port="${PGPORT}" \
-  --db_user="${PGUSER}" \
-  --db_password="${PGPASSWORD}"
+exec odoo \
+    --config=/etc/odoo/odoo.conf \
+    --db-filter=.* \
+    --addons-path=/usr/lib/python3/dist-packages/odoo/addons,/mnt/extra-addons \
+    --data-dir=/var/lib/odoo \
+    --db_host="${PGHOST}" \
+    --db_port="${PGPORT}" \
+    --db_user="${PGUSER}" \
+    --db_password="${PGPASSWORD}" \
+    --db_name="${PGDATABASE}"
 
